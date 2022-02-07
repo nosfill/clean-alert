2c10ffe9d80ec41bae1f3815ce55f64be86a3b7d
kitaoka tetsuya <kitaoka.tetsuya.ks9@is.naist.jp>
2022-02-07 14:21:01 +0900

    Add function that records this week cleaner to log

--- src/handle_channel.rs
+++ src/handle_channel.rs
@@ -1,3 +1,8 @@
+use chrono::{DateTime, Local};
+use std::error::Error;
+use std::fs::OpenOptions;
+
+
 #[derive(Debug)]
 pub struct SlackApp {
     token: String,
@@ -12,23 +17,23 @@ impl SlackApp {
         }
     }
 
-    pub fn get_channel_member_id(&self) -> Result<Vec<String>, ureq::Error> {
-        let url = "https://slack.com/api/conversations.members";
+    // pub fn get_channel_member_id(&self) -> Result<Vec<String>, ureq::Error> {
+    //     let url = "https://slack.com/api/conversations.members";
 
-        let resp = ureq::get(&url)
-            .set("Authorization", &format!("Bearer {}", &self.token))
-            .set("Content-Type", "application/x-www-form-urlencoded")
-            .query("channel", &self.channel)
-            .call()?
-            .into_json::<serde_json::Value>()?;
+    //     let resp = ureq::get(&url)
+    //         .set("Authorization", &format!("Bearer {}", &self.token))
+    //         .set("Content-Type", "application/x-www-form-urlencoded")
+    //         .query("channel", &self.channel)
+    //         .call()?
+    //         .into_json::<serde_json::Value>()?;
 
-        Ok(resp["members"]
-            .as_array()
-            .unwrap()
-            .iter()
-            .map(|x| x.as_str().unwrap().to_string())
-            .collect())
-    }
+    //     Ok(resp["members"]
+    //         .as_array()
+    //         .unwrap()
+    //         .iter()
+    //         .map(|x| x.as_str().unwrap().to_string())
+    //         .collect())
+    // }
 
     pub fn post_message(&self, text: &String) -> Result<ureq::Response, ureq::Error> {
         let url = "https://slack.com/api/chat.postMessage";
@@ -39,5 +44,19 @@ impl SlackApp {
             .send_json(
                 ureq::json!({ "channel": &self.channel, "text": text, "link_names": "true"}),
             )?)
+    }
+
+    pub fn record_cleaner(&self, members: Vec<String>) -> Result<(), Box<dyn Error>> {
+        let dt: DateTime<Local> = Local::now();
+        let mut record: Vec<String> = vec![dt.format("%F").to_string()];
+        record.append(&mut members.clone());
+
+        let file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open("./member_log.csv")?;
+        let mut wtr = csv::Writer::from_writer(file);
+        wtr.write_record(record)?;
+        Ok(())
     }
 }
